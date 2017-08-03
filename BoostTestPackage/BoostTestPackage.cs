54db871425b78a6427d87ed9fd03fbb960ff0ee7
Lukasz Mendakiewicz <lukaszme@microsoft.com>
2017-08-03 10:46:39 -0700

    Add VS package

---
+++ BoostTestPackage/BoostTestPackage.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace BoostTestAdapter
+{
+    /// <summary>
+    /// Implementation of AsyncPackage for Boost Tests.
+    /// </summary>
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string)]
+    [Guid(PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class BoostTestPackage : AsyncPackage
+    {
+        public const string PackageGuidString = "F6EA99EF-D84A-41DE-A751-A24F842B5946";
+
+        #region AsyncPackage
+        
+        /// <summary>
+        /// Initialize the package
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        protected override System.Threading.Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            return base.InitializeAsync(cancellationToken, progress);
+        }
+
+        #endregion
+    }
+}
